8f89d92e915905db280671f15ea1efda5a01c10b
kool814 <karthik.s814@gmail.com>
2019-04-16 14:47:43 -0400

    Adding flask server

--- application.py
+++ application.py
@@ -1,32 +1,31 @@
 from flask import Flask
 application = Flask(__name__)
+from flask import jsonify
+from flask import request
+import classifier
 
 @application.route("/")
 def hello():
     return "Hello World!"
 
-@application.route('/test', methods=['POST'])
-def test():
-    if request.method == 'POST':
-        classifiedProb = classifier.classifyArticle(articleTitle)
-        newsOrClickBait = ""
-        if classifiedProb['clickbait'] >= 0.5:
-            newsOrClickBait = '**CLICKBAIT**'
-        else:
-            newsOrClickBait = 'NEWS'
-
-        s = """({:.3f}% clickbait, {:.3f}% news) -> Article classified as {}""".format(
-            classifiedProb['clickbait'] * 100, 
-            classifiedProb['news'] * 100,
-            newsOrClickBait)
-        return s
-        # return do_the_login()
+@application.route('/runClassifier', methods=['POST'])
+def create_title():
+    if not request.json:
+        abort(400)
+    articleTitle = request.json['title']
+    classifiedProb = classifier.classifyArticle(articleTitle)
+    newsOrClickBait = ""
+    if classifiedProb['clickbait'] >= 0.5:
+        newsOrClickBait = 'CLICKBAIT'
     else:
-        return show_the_login_form()
+        newsOrClickBait = 'NEWS'
 
-# @application.route("/test")
-# def test():
-#     return "Test"
+    s = """({:.3f}% clickbait, {:.3f}% news) -> Article classified as {}\n""".format(
+        classifiedProb['clickbait'] * 100, 
+        classifiedProb['news'] * 100,
+        newsOrClickBait)
+    return s
 
 if __name__ == "__main__":
+    application.debug = True
     application.run()
